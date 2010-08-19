18f51b0e33510027ac677a03199d1a2698fc394f
liujunufinity <liujunufinity@559450ae-6f0b-01ad-8463-9f1cfce4235e>
2010-08-19 05:19:25 +0000

    add JsonUtil

---
+++ src/java/main/com/ufinity/marchant/ubank/common/JsonUtil.java
@@ -0,0 +1,247 @@
+//-------------------------------------------------------------------------
+// Copyright (c) 2000-2009 Ufinity. All Rights Reserved.
+//
+// This software is the confidential and proprietary information of
+// Ufinity
+//
+// Original author:
+//
+//-------------------------------------------------------------------------
+// UFINITY MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF
+// THE SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+// TO THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE, OR NON-INFRINGEMENT. UFINITY SHALL NOT BE
+// LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING,
+// MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES.
+//
+// THIS SOFTWARE IS NOT DESIGNED OR INTENDED FOR USE OR RESALE AS ON-LINE
+// CONTROL EQUIPMENT IN HAZARDOUS ENVIRONMENTS REQUIRING FAIL-SAFE
+// PERFORMANCE, SUCH AS IN THE OPERATION OF NUCLEAR FACILITIES, AIRCRAFT
+// NAVIGATION OR COMMUNICATION SYSTEMS, AIR TRAFFIC CONTROL, DIRECT LIFE
+// SUPPORT MACHINES, OR WEAPONS SYSTEMS, IN WHICH THE FAILURE OF THE
+// SOFTWARE COULD LEAD DIRECTLY TO DEATH, PERSONAL INJURY, OR SEVERE
+// PHYSICAL OR ENVIRONMENTAL DAMAGE ("HIGH RISK ACTIVITIES"). UFINITY
+// SPECIFICALLY DISCLAIMS ANY EXPRESS OR IMPLIED WARRANTY OF FITNESS FOR
+// HIGH RISK ACTIVITIES.
+//-------------------------------------------------------------------------
+
+package com.ufinity.marchant.ubank.common;
+
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.Map.Entry;
+
+/**
+ * @author caiweijia
+ */
+public class JsonUtil {
+    
+    /**
+     * object to change json
+     *
+     * @param obj
+     *            any object
+     * @return String
+     */
+    public static String object2json(Object obj) {
+        StringBuilder json = new StringBuilder();
+        if (obj == null) {
+            json.append("\"\"");
+        } else if (obj instanceof String || obj instanceof Integer
+                || obj instanceof Float || obj instanceof Boolean
+                || obj instanceof Short || obj instanceof Double
+                || obj instanceof Long || obj instanceof BigDecimal
+                || obj instanceof BigInteger || obj instanceof Byte) {
+            json.append("\"").append(string2json(obj.toString())).append("\"");
+        } else if (obj instanceof Object[]) {
+            json.append(array2json((Object[]) obj));
+        } else if (obj instanceof List) {
+            json.append(list2json((List<?>) obj));
+        } else if (obj instanceof Map) {
+            json.append(map2json((Map<?, ?>) obj));
+        } else if (obj instanceof Set) {
+            json.append(set2json((Set<?>) obj));
+        } else {
+            json.append(bean2json(obj));
+        }
+        return json.toString();
+    }
+
+    /**
+     * bean to change json
+     * @param bean  
+     *            any Bean
+     * @return String
+     */
+    public static String bean2json(Object bean) {
+        StringBuilder json = new StringBuilder();
+        json.append("{");
+        PropertyDescriptor[] props = null;
+        try {
+            props = Introspector.getBeanInfo(bean.getClass(), Object.class)
+                    .getPropertyDescriptors();
+        } catch (IntrospectionException e) {
+        }
+        if (props != null) {
+            for (int i = 0; i < props.length; i++) {
+                try {
+                    String name = object2json(props[i].getName());
+                    String value = object2json(props[i].getReadMethod().invoke(
+                            bean));
+                    json.append(name);
+                    json.append(":");
+                    json.append(value);
+                    json.append(",");
+                } catch (Exception e) {
+                }
+            }
+            json.setCharAt(json.length() - 1, '}');
+        } else {
+            json.append("}");
+        }
+        return json.toString();
+    }
+
+    /**
+     * List to change json
+     * @param list  
+     *            any List
+     * @return String
+     */
+    public static String list2json(List<?> list) {
+        StringBuilder json = new StringBuilder();
+        json.append("[");
+        if (list != null && list.size() > 0) {
+            for (Object obj : list) {
+                json.append(object2json(obj));
+                json.append(",");
+            }
+            json.setCharAt(json.length() - 1, ']');
+        } else {
+            json.append("]");
+        }
+        return json.toString();
+    }
+
+    /**
+     * Array to change json
+     * @param array  
+     *             any Array
+     * @return String
+     */
+    public static String array2json(Object[] array) {
+        StringBuilder json = new StringBuilder();
+        json.append("[");
+        if (array != null && array.length > 0) {
+            for (Object obj : array) {
+                json.append(object2json(obj));
+                json.append(",");
+            }
+            json.setCharAt(json.length() - 1, ']');
+        } else {
+            json.append("]");
+        }
+        return json.toString();
+    }
+
+    /**
+     * map to change json
+     * 
+     * @param map  
+     *             any Map
+     * @return String
+     */
+    public static String map2json(Map<?, ?> map) {
+        StringBuilder json = new StringBuilder();
+        json.append("{");
+        if (map != null && map.size() > 0) {
+            for(Entry<?, ?>  entry : map.entrySet()){
+                json.append(object2json(entry.getKey()));
+                json.append(":");
+                json.append(object2json(entry.getValue()));
+                json.append(",");
+            }
+            json.setCharAt(json.length() - 1, '}');
+        } else {
+            json.append("}");
+        }
+        return json.toString();
+    }
+
+    /**
+     * Set to change json
+     * @param set  
+     *           any Set
+     * @return String
+     */
+    public static String set2json(Set<?> set) {
+        StringBuilder json = new StringBuilder();
+        json.append("[");
+        if (set != null && set.size() > 0) {
+            for (Object obj : set) {
+                json.append(object2json(obj));
+                json.append(",");
+            }
+            json.setCharAt(json.length() - 1, ']');
+        } else {
+            json.append("]");
+        }
+        return json.toString();
+    }
+
+    /**
+     * String to change json
+     * @param s  
+     *           any Stirng
+     * @return String
+     */
+    public static String string2json(String s) {
+        if (s == null)
+            return "";
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            switch (ch) {
+            case '"':
+                sb.append("\\\"");
+                break;
+            case '\\':
+                sb.append("\\\\");
+                break;
+            case '\b':
+                sb.append("\\b");
+                break;
+            case '\f':
+                sb.append("\\f");
+                break;
+            case '\n':
+                sb.append("\\n");
+                break;
+            case '\r':
+                sb.append("\\r");
+                break;
+            case '\t':
+                sb.append("\\t");
+                break;
+            default:
+                if (ch >= '\u0000' && ch <= '\u001F') {
+                    String ss = Integer.toHexString(ch);
+                    sb.append("\\u");
+                    for (int k = 0; k < 4 - ss.length(); k++) {
+                        sb.append('0');
+                    }
+                    sb.append(ss.toUpperCase());
+                } else {
+                    sb.append(ch);
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
