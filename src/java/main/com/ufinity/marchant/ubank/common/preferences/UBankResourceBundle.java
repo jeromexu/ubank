3620a0b79061925901adea55e5b0111151bfbfbc
xuezhongde <xuezhongde@559450ae-6f0b-01ad-8463-9f1cfce4235e>
2010-08-20 02:06:37 +0000

    commit:  for add class ufinity license

--- src/java/main/com/ufinity/marchant/ubank/common/preferences/UBankResourceBundle.java
+++ src/java/main/com/ufinity/marchant/ubank/common/preferences/UBankResourceBundle.java
@@ -1,3 +1,26 @@
+// -------------------------------------------------------------------------
+// Copyright (c) 2000-2009 Ufinity. All Rights Reserved.
+//
+// This software is the confidential and proprietary information of
+// Ufinity
+// -------------------------------------------------------------------------
+// UFINITY MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF
+// THE SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+// TO THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE, OR NON-INFRINGEMENT. UFINITY SHALL NOT BE
+// LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING,
+// MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES.
+//
+// THIS SOFTWARE IS NOT DESIGNED OR INTENDED FOR USE OR RESALE AS ON-LINE
+// CONTROL EQUIPMENT IN HAZARDOUS ENVIRONMENTS REQUIRING FAIL-SAFE
+// PERFORMANCE, SUCH AS IN THE OPERATION OF NUCLEAR FACILITIES, AIRCRAFT
+// NAVIGATION OR COMMUNICATION SYSTEMS, AIR TRAFFIC CONTROL, DIRECT LIFE
+// SUPPORT MACHINES, OR WEAPONS SYSTEMS, IN WHICH THE FAILURE OF THE
+// SOFTWARE COULD LEAD DIRECTLY TO DEATH, PERSONAL INJURY, OR SEVERE
+// PHYSICAL OR ENVIRONMENTAL DAMAGE ("HIGH RISK ACTIVITIES"). UFINITY
+// SPECIFICALLY DISCLAIMS ANY EXPRESS OR IMPLIED WARRANTY OF FITNESS FOR
+// HIGH RISK ACTIVITIES.
+// -------------------------------------------------------------------------
 package com.ufinity.marchant.ubank.common.preferences;
 
 import java.util.Locale;
@@ -13,8 +36,10 @@ public class UBankResourceBundle {
     private ResourceBundle rb;
 
     /**
-     * Constructor
+     * Constructor with baseName and locale
      * 
+     * @param baseName config file base
+     * @param locale locale
      * @author zdxue
      */
     public UBankResourceBundle(String baseName, Locale locale) {
@@ -26,9 +51,9 @@ public class UBankResourceBundle {
     }
 
     /**
-     * Get string value by key, if cannot get config value, then return empty string
+     * Get value by key, if cannot get config value, then return empty string
      *
-     * @param key key
+     * @param key the given key
      * @return value
      * @author zdxue
      */
--- src/java/main/com/ufinity/marchant/ubank/filter/CharsetFilter.java
+++ src/java/main/com/ufinity/marchant/ubank/filter/CharsetFilter.java
@@ -1,6 +1,30 @@
+// -------------------------------------------------------------------------
+// Copyright (c) 2000-2009 Ufinity. All Rights Reserved.
+//
+// This software is the confidential and proprietary information of
+// Ufinity
+// -------------------------------------------------------------------------
+// UFINITY MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF
+// THE SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+// TO THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE, OR NON-INFRINGEMENT. UFINITY SHALL NOT BE
+// LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING,
+// MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES.
+//
+// THIS SOFTWARE IS NOT DESIGNED OR INTENDED FOR USE OR RESALE AS ON-LINE
+// CONTROL EQUIPMENT IN HAZARDOUS ENVIRONMENTS REQUIRING FAIL-SAFE
+// PERFORMANCE, SUCH AS IN THE OPERATION OF NUCLEAR FACILITIES, AIRCRAFT
+// NAVIGATION OR COMMUNICATION SYSTEMS, AIR TRAFFIC CONTROL, DIRECT LIFE
+// SUPPORT MACHINES, OR WEAPONS SYSTEMS, IN WHICH THE FAILURE OF THE
+// SOFTWARE COULD LEAD DIRECTLY TO DEATH, PERSONAL INJURY, OR SEVERE
+// PHYSICAL OR ENVIRONMENTAL DAMAGE ("HIGH RISK ACTIVITIES"). UFINITY
+// SPECIFICALLY DISCLAIMS ANY EXPRESS OR IMPLIED WARRANTY OF FITNESS FOR
+// HIGH RISK ACTIVITIES.
+// -------------------------------------------------------------------------
 package com.ufinity.marchant.ubank.filter;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -14,11 +38,17 @@ import com.ufinity.marchant.ubank.common.Constant;
 
 /**
  * Charset filter
- *
+ * 
+ * <p>
+ *  Web request content charset filter, it get the specified encode from web.xml filter's init-params,
+ *  if the given charset not be supported, the set the default charset be utf-8. 
+ * </p>
+ * 
  * @version 1.0 - 2010-8-19
  * @author zdxue     
  */
 public class CharsetFilter implements Filter {
+    private static final String DEF_ENCODE = "utf-8";
     private String enable;
     private String encode;
 
@@ -36,9 +66,14 @@ public class CharsetFilter implements Filter {
      */
     public void doFilter(ServletRequest req, ServletResponse resp,
             FilterChain chain) throws IOException, ServletException {
+        
         if(Constant.ENABLE_FILTER.equals(enable)) {
             HttpServletRequest request = (HttpServletRequest)req;
-            request.setCharacterEncoding(encode);
+            try{
+                request.setCharacterEncoding(encode);
+            }catch(UnsupportedEncodingException e) {
+                request.setCharacterEncoding(DEF_ENCODE);
+            }
         }
         
         chain.doFilter(req, resp);
