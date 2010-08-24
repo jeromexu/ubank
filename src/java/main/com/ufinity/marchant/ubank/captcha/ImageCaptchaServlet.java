2b999582cf8766860dfbfb0771918a4e922ea109
gphonebbs <gphonebbs@559450ae-6f0b-01ad-8463-9f1cfce4235e>
2010-08-24 02:14:34 +0000

    delete the default constructor

--- src/java/main/com/ufinity/marchant/ubank/captcha/ImageCaptchaServlet.java
+++ src/java/main/com/ufinity/marchant/ubank/captcha/ImageCaptchaServlet.java
@@ -15,14 +15,6 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class ImageCaptchaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-	/**
-	 * Default constructor.
-	 */
-	public ImageCaptchaServlet() {
-		// TODO Auto-generated constructor stub
-	}
-
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
 	 *      response)
@@ -31,16 +23,14 @@ public class ImageCaptchaServlet extends HttpServlet {
 			HttpServletResponse response) throws ServletException, IOException {
 		doPost(request, response);
 	}
-
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-		
+		// gernate the captcha code
 		MyCaptchaService.getInstance().writeCaptchaImage(request, response);   
-
 	}
 
 }
