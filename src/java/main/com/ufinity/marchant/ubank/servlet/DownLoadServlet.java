06a66949f6109c4643c9bae11584c5fe0ad4f607
gphonebbs <gphonebbs@559450ae-6f0b-01ad-8463-9f1cfce4235e>
2010-08-26 09:19:21 +0000

    set the file name charset

--- src/java/main/com/ufinity/marchant/ubank/servlet/DownLoadServlet.java
+++ src/java/main/com/ufinity/marchant/ubank/servlet/DownLoadServlet.java
@@ -5,7 +5,11 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -26,13 +30,14 @@ import com.ufinity.marchant.ubank.service.ServiceFactory;
  * @since 2010-8-19
  */
 public class DownLoadServlet extends AbstractServlet {
-	
+
 	private static final long serialVersionUID = 1L;
 	// Logger for this class
-    protected static  final Logger LOGGER = Logger.getLogger(DownLoadServlet.class);
-    
-    // file service business logic instance
-    private FileService fileService = null;
+	protected static final Logger LOGGER = Logger
+			.getLogger(DownLoadServlet.class);
+
+	// file service business logic instance
+	private FileService fileService = null;
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
@@ -49,81 +54,84 @@ public class DownLoadServlet extends AbstractServlet {
 	 */
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-		
+
 		String method = parseActionName(request);
-        String rslt = null;
-        if(Constant.ACTION_DOWNLOAD.equals(method)){
-            rslt = download(request, response);
-        }
-        if(rslt != null){
-        	forward(request, response, rslt);
-        }
+		String rslt = null;
+		if (Constant.ACTION_DOWNLOAD.equals(method)) {
+			rslt = download(request, response);
+		}
+		if (rslt != null) {
+			forward(request, response, rslt);
+		}
 	}
 
 	/**
 	 * 
 	 * download the file
 	 * 
-	 * @param request download file
-	 * @param response 
+	 * @param request
+	 *            download file
+	 * @param response
 	 * @author jerome
 	 */
 	@SuppressWarnings("deprecation")
 	private String download(HttpServletRequest request,
 			HttpServletResponse response) {
-		
-		response.setContentType("text/html; charset=UTF-8");
-		String id = request.getParameter("id");
-		Long fileId = 0l;
-		if (!Validity.isEmpty(id)) {
-			fileId = Long.valueOf(id);
-		} else {
-			return Constant.ERROR_PAGE;
-		}
-		fileService = ServiceFactory.createService(FileService.class);
-		FileBean fileBean = null;
-		try {
-			fileBean = fileService.getFileBean(fileId);
-		} catch (UBankException e1) {
-			LOGGER.error("get the file exception!", e1);
-		}
-		File file =  null;
-		if (fileBean != null) {
-			// create file object
-	        file = new File(request.getRealPath("/")+getFilePath(fileBean.getDirectory(),fileBean.getFileName()));
-	        
-	        // set response encoded mode 
-	        response.setContentType("application/x-msdownload");
-
-	        // set the file size 
-	        response.setContentLength((int) file.length());
-
-	        // set the addition file name
-	        response.setHeader("Content-Disposition","attachment;filename="+fileBean.getFileName());
-		} else {
-			return Constant.ERROR_PAGE;
-		}
 		BufferedInputStream buff = null;
 		OutputStream outPut = null;
 		try {
-			 // read file into i/o
-			 buff = new BufferedInputStream(new FileInputStream(file));
-			 // cache data
-		     byte [] b = new byte[5*1024];
-		     // count the file size which file has downloaded currently
-		     long k = 0;
-		     outPut = response.getOutputStream();
-		     while(k < file.length()){
-		          int j = buff.read(b,0,1024);
-		          k += j;
-		          //write the data into the client memory
-		          outPut.write(b,0,j);
-		     }
-		     outPut.flush();
+			response.setContentType("text/html; charset=UTF-8");
+			String id = request.getParameter("id");
+			Long fileId = 0l;
+			if (!Validity.isEmpty(id)) {
+				fileId = Long.valueOf(id);
+			} else {
+				return Constant.ERROR_PAGE;
+			}
+			fileService = ServiceFactory.createService(FileService.class);
+			FileBean fileBean = fileService.getFileBean(fileId);
+			File file = null;
+			if (fileBean != null) {
+				// create file object
+				file = new File(request.getRealPath("/")
+						+ getFilePath(fileBean.getDirectory(), fileBean
+								.getFileName()));
+
+				// set response encoded mode
+				response.setContentType("application/x-msdownload");
+				// set the file size
+				response.setContentLength((int) file.length());
+
+				// set the addition file name
+				// set the file charset
+				response.setHeader("Content-Disposition",
+						"attachment;filename="
+								+ new String(fileBean.getFileName().getBytes(
+										"gb2312"), "iso-8859-1"));
+				// response.setHeader("Content-Disposition","attachment;filename="+
+				// URLEncoder.encode(fileBean.getFileName(),"UTF-8"));
+			} else {
+				return Constant.ERROR_PAGE;
+			}
+			// read file into i/o
+			buff = new BufferedInputStream(new FileInputStream(file));
+			// cache data
+			byte[] b = new byte[5 * 1024];
+			// count the file size which file has downloaded currently
+			long k = 0;
+			outPut = response.getOutputStream();
+			while (k < file.length()) {
+				int j = buff.read(b, 0, 1024);
+				k += j;
+				// write the data into the client memory
+				outPut.write(b, 0, j);
+			}
+			// refresh the file content into the disk
+			outPut.flush();
 		} catch (Exception e) {
 			LOGGER.error("download file exception!", e);
-		} finally{
-			if(buff != null){
+		} finally {
+			if (buff != null) {
 				try {
 					buff.close();
 				} catch (IOException e) {
@@ -140,17 +148,20 @@ public class DownLoadServlet extends AbstractServlet {
 		}
 		return null;
 	}
+
 	/**
 	 * get the integrited path
 	 * 
-	 * @param directory the file path folder
-	 * @param fileName  the file name
-	 * @return integrited path 
+	 * @param directory
+	 *            the file path folder
+	 * @param fileName
+	 *            the file name
+	 * @return integrited path
 	 * @author jerome
 	 */
-	private String getFilePath(String directory, String fileName){
+	private String getFilePath(String directory, String fileName) {
 		String filePath = null;
-		if(!Validity.isEmpty(directory) && directory.endsWith(File.separator)){
+		if (!Validity.isEmpty(directory) && directory.endsWith(File.separator)) {
 			filePath = directory + fileName;
 		} else {
 			filePath = directory + File.separator + fileName;
