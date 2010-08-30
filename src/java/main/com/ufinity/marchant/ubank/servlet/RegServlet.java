aa2ea98a6d756bdf7a62c2e15088ce17b6481049
gphonebbs <gphonebbs@559450ae-6f0b-01ad-8463-9f1cfce4235e>
2010-08-30 02:41:41 +0000

    add the length judgement

--- src/java/main/com/ufinity/marchant/ubank/servlet/RegServlet.java
+++ src/java/main/com/ufinity/marchant/ubank/servlet/RegServlet.java
@@ -27,112 +27,114 @@ import com.ufinity.marchant.ubank.service.UserService;
  */
 public class RegServlet extends AbstractServlet {
 
-    private static final long serialVersionUID = 1L;
-    // Logger for this class
-    protected final Logger logger = Logger.getInstance(RegServlet.class);
+	private static final long serialVersionUID = 1L;
+	// Logger for this class
+	protected final Logger logger = Logger.getInstance(RegServlet.class);
 
-    // user service business logic instance
-    private UserService userService = null;
+	// user service business logic instance
+	private UserService userService = null;
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-     *      response)
-     */
-    protected void doGet(HttpServletRequest request,
-            HttpServletResponse response) throws ServletException, IOException {
-        doPost(request, response);
-    }
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
 
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-     *      response)
-     */
-    protected void doPost(HttpServletRequest request,
-            HttpServletResponse response) throws ServletException, IOException {
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 
-        String method = parseActionName(request);
-        String rslt = Constant.ERROR_PAGE;
-        if (Constant.ACTION_REGISTER.equals(method)) {
-            rslt = register(request);
-        }
-        forward(request, response, rslt);
-    }
+		String method = parseActionName(request);
+		String rslt = Constant.ERROR_PAGE;
+		if (Constant.ACTION_REGISTER.equals(method)) {
+			rslt = register(request);
+		}
+		forward(request, response, rslt);
+	}
 
-    /**
-     * display register
-     * 
-     * @param request
-     *            req
-     * @param response
-     *            res
-     * @author jerome
-     */
-    private String register(HttpServletRequest request) {
+	/**
+	 * display register
+	 * 
+	 * @param request
+	 *            req
+	 * @param response
+	 *            res
+	 * @author jerome
+	 */
+	private String register(HttpServletRequest request) {
 
-        try {
-            String captchaCode = request
-                    .getParameter(Constant.REQ_PARAM_CAPTCHACODE);
-            logger.debug(" request captchaCode = " + captchaCode);
-            if (Validity.isNullAndEmpty(captchaCode)) {
-                request.setAttribute(Constant.ATTR_ERROR_MSG,
-                        getText(MessageKeys.CAPTCHA_ERR_MSG));
-                return Constant.REGISTER_PAGE;
-            }
-            HttpSession session = request.getSession();
-            boolean isValidateCode = false;
-            // retrieve the response
-            String validateCode = captchaCode.trim();
-            isValidateCode = MyCaptchaService.getInstance()
-                    .validateCaptchaResponse(validateCode, session);
-            if (!isValidateCode) {
-                // captcha code is not right
-                request.setAttribute(Constant.ATTR_ERROR_MSG,
-                        getText(MessageKeys.CAPTCHA_ERR_MSG));
-            }
-            else {
-                String userName = request
-                        .getParameter(Constant.REQ_PARAM_USERNAME);
-                String pass = request.getParameter(Constant.REQ_PARAM_PASSWORD);
-                String repass = request
-                        .getParameter(Constant.REQ_PARAM_REPASSWORD);
-                logger.debug("userName = " + userName + ",pass = " + pass
-                        + ",repass = " + repass);
-                if (Validity.isNullAndEmpty(userName)) {
-                    request.setAttribute(Constant.ATTR_ERROR_MSG,
-                            getText(MessageKeys.USERNAME_ERR_MSG));
-                    return Constant.REGISTER_PAGE;
-                }
-                if (Validity.isNullAndEmpty(pass)) {
-                    request.setAttribute(Constant.ATTR_ERROR_MSG,
-                            getText(MessageKeys.PASS_ERR_MSG));
-                    return Constant.REGISTER_PAGE;
-                }
-                else if (!pass.equals(repass)) {
-                    request.setAttribute(Constant.ATTR_ERROR_MSG,
-                            getText(MessageKeys.REPASS_ERR_MSG));
-                    return Constant.REGISTER_PAGE;
-                }
-                User user = new User();
-                user.setUserName(userName);
-                user.setPassword(pass);
-                user.setCreateTime(new Date());
-                user.setOverSize(SystemGlobals
-                        .getInt(ConfigKeys.DEFAULT_USER_SPACE_SIZE));
-                userService = ServiceFactory.createService(UserService.class);
-                String registerMsg = userService.doRegister(user);
-                if (MessageKeys.REGISTER_FAILURE.equals(registerMsg)) {
-                	logger.debug(userName + " register failure!");
-                } else {
-                	logger.debug(userName + " register success!");
-                }
-                request.setAttribute(Constant.REGISTER_MSG, registerMsg);
-            }
-        }
-        catch (Exception e) {
-            logger.error("register exception message!", e);
-            request.setAttribute(Constant.ATTR_ERROR_MSG, getText(MessageKeys.REGISTER_EXCEPTION));
-        }
-        return Constant.REGISTER_PAGE;
-    }
+		try {
+			String captchaCode = request
+					.getParameter(Constant.REQ_PARAM_CAPTCHACODE);
+			logger.debug(" request captchaCode = " + captchaCode);
+			if (Validity.isNullAndEmpty(captchaCode)) {
+				request.setAttribute(Constant.ATTR_ERROR_MSG,
+						getText(MessageKeys.CAPTCHA_ERR_MSG));
+				return Constant.REGISTER_PAGE;
+			}
+			HttpSession session = request.getSession();
+			boolean isValidateCode = false;
+			// retrieve the response
+			String validateCode = captchaCode.trim();
+			isValidateCode = MyCaptchaService.getInstance()
+					.validateCaptchaResponse(validateCode, session);
+			if (!isValidateCode) {
+				// captcha code is not right
+				request.setAttribute(Constant.ATTR_ERROR_MSG,
+						getText(MessageKeys.CAPTCHA_ERR_MSG));
+			} else {
+				String userName = request
+						.getParameter(Constant.REQ_PARAM_USERNAME);
+				String pass = request.getParameter(Constant.REQ_PARAM_PASSWORD);
+				String repass = request
+						.getParameter(Constant.REQ_PARAM_REPASSWORD);
+				logger.debug("userName = " + userName + ",pass = " + pass
+						+ ",repass = " + repass);
+				if (Validity.isNullAndEmpty(userName)
+						&& !(Validity.isLessLength(userName,
+								Constant.USERNAME_LENGTH))) {
+					request.setAttribute(Constant.ATTR_ERROR_MSG,
+							getText(MessageKeys.USERNAME_ERR_MSG));
+					return Constant.REGISTER_PAGE;
+				}
+				if (Validity.isNullAndEmpty(pass)
+						&& !(Validity.isLessLength(pass,
+								Constant.PASSWORD_LENGTH))) {
+					request.setAttribute(Constant.ATTR_ERROR_MSG,
+							getText(MessageKeys.PASS_ERR_MSG));
+					return Constant.REGISTER_PAGE;
+				} else if (!pass.equals(repass)) {
+					request.setAttribute(Constant.ATTR_ERROR_MSG,
+							getText(MessageKeys.REPASS_ERR_MSG));
+					return Constant.REGISTER_PAGE;
+				}
+				User user = new User();
+				user.setUserName(userName);
+				user.setPassword(pass);
+				user.setCreateTime(new Date());
+				user.setOverSize(SystemGlobals
+						.getInt(ConfigKeys.DEFAULT_USER_SPACE_SIZE));
+				userService = ServiceFactory.createService(UserService.class);
+				String registerMsg = userService.doRegister(user);
+				if (MessageKeys.REGISTER_FAILURE.equals(registerMsg)) {
+					logger.debug(userName + " register failure!");
+				} else {
+					logger.debug(userName + " register success!");
+				}
+				request.setAttribute(Constant.REGISTER_MSG, registerMsg);
+			}
+		} catch (Exception e) {
+			logger.error("register exception message!", e);
+			request.setAttribute(Constant.ATTR_ERROR_MSG,
+					getText(MessageKeys.REGISTER_EXCEPTION));
+		}
+		return Constant.REGISTER_PAGE;
+	}
 
 }
