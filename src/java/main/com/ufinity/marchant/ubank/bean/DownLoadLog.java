882570981c1b7e67140868df2ebd2e6ba443059c
skyqiang2009 <skyqiang2009@559450ae-6f0b-01ad-8463-9f1cfce4235e>
2010-09-02 06:56:12 +0000

    add find download log according user'id and file'id method.

--- src/java/main/com/ufinity/marchant/ubank/bean/DownLoadLog.java
+++ src/java/main/com/ufinity/marchant/ubank/bean/DownLoadLog.java
@@ -36,6 +36,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -46,6 +48,7 @@ import javax.persistence.TemporalType;
  */
 @Entity
 @Table(name = "U_DOWNLOAD_LOG")
+@NamedQueries( { @NamedQuery(name = "DownLoadLog.findDownLoadLog", query = "SELECT d FROM DownLoadLog d WHERE d.user.userId = :userId AND d.file.fileId = :fileId") })
 public class DownLoadLog {
 
     private Long downLoadLogId;
